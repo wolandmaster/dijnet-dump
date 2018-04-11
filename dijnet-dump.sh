79eb4a09a65c780ec5316d78ee4a97e88d109053
Krisztian VASAS <639399+ironiq@users.noreply.github.com>
2018-04-11 20:52:29 +0200

    Changed INVOICE_NUMBER: changed order of date and real invoice number

--- dijnet-dump.sh
+++ dijnet-dump.sh
@@ -82,7 +82,7 @@ for ID in $(echo "${PROVIDERS}" | xpath '//option/@value' | sed 's/value="\([^"]
   for INVOICE in ${INVOICES}; do
     dijnet "control/${INVOICE}" | iconv -f iso8859-2 -t utf-8 | grep -q 'href="szamla_letolt"' || die
     INVOICE_DOWNLOAD=$(dijnet "control/szamla_letolt")
-    INVOICE_NUMBER=$(echo "${INVOICE_DOWNLOAD}" | xpath '//label[@class="title_next_s"]/text()' | sed 's/\//_/g;s/ //g')
+    INVOICE_NUMBER=$(echo "${INVOICE_DOWNLOAD}" | xpath '//label[@class="title_next_s"]/text()' | sed 's/\//_/g;s/ //g; s@\(.*\)-\(.*\)@\2-\1@g')
     TARGET_FOLDER=$(echo "${PROVIDER}/${INVOICE_NUMBER}" | sed 's/ \+/_/g;s/\.\//\//g')
     mkdir -p "${TARGET_FOLDER}" || die "not able to create folder: ${TARGET_FOLDER}"
     echo "${INVOICE_INDEX}"
