f8099de782b0c208c2256d622a7b754d3f353281
Krisztian VASAS <639399+ironiq@users.noreply.github.com>
2018-04-11 20:55:05 +0200

    Added possibility to download files to different directory

--- dijnet-dump.sh
+++ dijnet-dump.sh
@@ -48,6 +48,7 @@ fi
 
 SCRIPT=$(basename $0)
 DIJNET_BASE_URL="https://www.dijnet.hu/ekonto"
+BASEDIR=${base:-.}
 
 USER="$1"; PASS="$2"
 
@@ -83,7 +84,7 @@ for ID in $(echo "${PROVIDERS}" | xpath '//option/@value' | sed 's/value="\([^"]
     dijnet "control/${INVOICE}" | iconv -f iso8859-2 -t utf-8 | grep -q 'href="szamla_letolt"' || die
     INVOICE_DOWNLOAD=$(dijnet "control/szamla_letolt")
     INVOICE_NUMBER=$(echo "${INVOICE_DOWNLOAD}" | xpath '//label[@class="title_next_s"]/text()' | sed 's/\//_/g;s/ //g; s@\(.*\)-\(.*\)@\2-\1@g')
-    TARGET_FOLDER=$(echo "${PROVIDER}/${INVOICE_NUMBER}" | sed 's/ \+/_/g;s/\.\//\//g')
+    TARGET_FOLDER=$(echo "${BASEDIR}/${PROVIDER}/${INVOICE_NUMBER}" | sed 's/ \+/_/g;s/\.\//\//g')
     mkdir -p "${TARGET_FOLDER}" || die "not able to create folder: ${TARGET_FOLDER}"
     echo "${INVOICE_INDEX}"
     DOWNLOAD_LINKS=$(echo "${INVOICE_DOWNLOAD}" | xpath '//a[contains(@class, "xt_link__download")]/@href' | sed 's/href="\([^"]*\)"/\1 /g')
